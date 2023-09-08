c836536e481dac082024265e9ce31c6407c8aad4
Rishi <48538864+DevRishii@users.noreply.github.com>
2023-09-07 21:48:56 -0400

    Added multithreading for getting item info

--- steamWSGameInfoScraper.py
+++ steamWSGameInfoScraper.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 import pandas as pd
+import threading
 
 # Returns a list of game URLs from the Steam Workshop
 def getGameUrls(driver):
@@ -98,120 +99,176 @@ def getItems(driver, tabUrl):
 # Collects item info and adds it to db
 def getItemInfo(driver, itemUrl, df, numItems):
     driver.get(itemUrl)
-    #get game name
-    gameName = driver.find_element(By.CLASS_NAME, 'workshopItemTitle').text
-    print("gameName:", gameName)
-    #get game id
-    gameId = driver.find_element(By.CLASS_NAME, 'breadcrumbs').find_elements(By.TAG_NAME, 'a')[2].get_attribute('href').split('appid=')[-1]
-    print("gameId:", gameId)
-    #get game link
-    gameLink = driver.find_element(By.CLASS_NAME, 'breadcrumbs').find_elements(By.TAG_NAME, 'a')[1].get_attribute('href')
-    print("gameLink:", gameLink)
-    #get number of items
-    noItems = numItems
-    print("noItems:", noItems)
-    #get number of RTU items
-    noRTUItems = numItems #not present on all pages
-    print("noRTUItems:", noRTUItems)
-    #get item name
-    itemName = driver.find_element(By.CLASS_NAME, 'workshopItemTitle').text
-    print("itemName:", itemName)
-    #get created by
-    createdBy = driver.find_element(By.CLASS_NAME, 'friendBlockContent').text
-    print("createdBy:", createdBy)
+    while True:
+        try:
+            #get game name
+            gameName = driver.find_element(By.CLASS_NAME, 'workshopItemTitle').text
+            print("gameName:", gameName)
+            #get game id
+            gameId = driver.find_element(By.CLASS_NAME, 'breadcrumbs').find_elements(By.TAG_NAME, 'a')[2].get_attribute('href').split('appid=')[-1]
+            print("gameId:", gameId)
+            #get game link
+            gameLink = driver.find_element(By.CLASS_NAME, 'breadcrumbs').find_elements(By.TAG_NAME, 'a')[1].get_attribute('href')
+            print("gameLink:", gameLink)
+            #get number of items
+            noItems = numItems
+            print("noItems:", noItems)
+            #get number of RTU items
+            noRTUItems = numItems #not present on all pages
+            print("noRTUItems:", noRTUItems)
+            #get item name
+            itemName = driver.find_element(By.CLASS_NAME, 'workshopItemTitle').text
+            print("itemName:", itemName)
+            #get created by
+            createdBy = driver.find_element(By.CLASS_NAME, 'friendBlockContent').text
+            print("createdBy:", createdBy)
 
-    #get item size, posted time, updated time
-    details = driver.find_elements(By.CLASS_NAME, 'detailsStatRight')
+            #get item size, posted time, updated time
+            details = driver.find_elements(By.CLASS_NAME, 'detailsStatRight')
 
-    #get item size
-    itemSize = details[0].text
-    print("itemSize:", itemSize)
-    #get posted time
-    postedTime = details[1].text
-    print("postedTime:", postedTime)
-    #get updated time
-    if len(details) == 3:
-        updatedTime = details[2].text
-    else:
-        updatedTime = 'N/A'
-    print("updatedTime:", updatedTime)
-    #get item description
-    itemDesc = driver.find_element(By.CLASS_NAME, 'workshopItemDescription').text
-    print("itemDesc:", itemDesc)
-    #get is curated
-    # isCurated = driver.find_element(By.CLASS_NAME, 'detailsStatRight').text
-    isCurated = 'N/A' #not present on all pages
-    print("isCurated:", isCurated)
-    #get is RTU
-    isRTU = 'N/A' #not present on all pages
-    print("isRTU:", isRTU)
-    #get number of unique visitors
-    noUniqVis = driver.find_element(By.XPATH, '//*[@id="rightContents"]/div/div[2]/table/tbody/tr[1]/td[1]').text
-    print("noUniqVis:", noUniqVis)
-    #get number of favorites
-    nofavs = driver.find_element(By.XPATH, '//*[@id="rightContents"]/div/div[2]/table/tbody/tr[3]/td[1]').text
-    print("nofavs:", nofavs)
-    #get number of subscriptions
-    noSubs = driver.find_element(By.XPATH, '//*[@id="rightContents"]/div/div[2]/table/tbody/tr[2]/td[1]').text
-    print("noSubs:", noSubs)
-    #get reviews
-    ratingLink = driver.find_element(By.CSS_SELECTOR, '#detailsHeaderRight > div > div > img').get_attribute('src').split('/')[-1]
+            #get item size
+            itemSize = details[0].text
+            print("itemSize:", itemSize)
+            #get posted time
+            postedTime = details[1].text
+            print("postedTime:", postedTime)
+            #get updated time
+            if len(details) == 3:
+                updatedTime = details[2].text
+            else:
+                updatedTime = 'N/A'
+            print("updatedTime:", updatedTime)
+            #get item description
+            itemDesc = driver.find_element(By.CLASS_NAME, 'workshopItemDescription').text
+            print("itemDesc:", itemDesc)
+            #get is curated
+            # isCurated = driver.find_element(By.CLASS_NAME, 'detailsStatRight').text
+            isCurated = 'N/A' #not present on all pages
+            print("isCurated:", isCurated)
+            #get is RTU
+            isRTU = 'N/A' #not present on all pages
+            print("isRTU:", isRTU)
+            #get number of unique visitors
+            noUniqVis = driver.find_element(By.XPATH, '//*[@id="rightContents"]/div/div[2]/table/tbody/tr[1]/td[1]').text
+            print("noUniqVis:", noUniqVis)
+            #get number of favorites
+            nofavs = driver.find_element(By.XPATH, '//*[@id="rightContents"]/div/div[2]/table/tbody/tr[3]/td[1]').text
+            print("nofavs:", nofavs)
+            #get number of subscriptions
+            noSubs = driver.find_element(By.XPATH, '//*[@id="rightContents"]/div/div[2]/table/tbody/tr[2]/td[1]').text
+            print("noSubs:", noSubs)
+            #get reviews
+            ratingLink = driver.find_element(By.CSS_SELECTOR, '#detailsHeaderRight > div > div > img').get_attribute('src').split('/')[-1]
 
-    if ratingLink == '1-star_large.png?v=2':
-        rating = '1'
-    elif ratingLink == '2-star_large.png?v=2':
-        rating = '2'
-    elif ratingLink == '3-star_large.png?v=2':
-        rating = '3'
-    elif ratingLink == '4-star_large.png?v=2':
-        rating = '4'
-    elif ratingLink == '5-star_large.png?v=2':
-        rating = '5'
-    else:
-        rating = 'N/A'
-    print("ratinglink:", ratingLink)
-    print('rating:', rating)
+            if ratingLink == '1-star_large.png?v=2':
+                rating = '1'
+            elif ratingLink == '2-star_large.png?v=2':
+                rating = '2'
+            elif ratingLink == '3-star_large.png?v=2':
+                rating = '3'
+            elif ratingLink == '4-star_large.png?v=2':
+                rating = '4'
+            elif ratingLink == '5-star_large.png?v=2':
+                rating = '5'
+            else:
+                rating = 'N/A'
+            print('rating:', rating)
+            break
+        except selenium.common.exceptions.StaleElementReferenceException:
+            print("StaleElementReferenceException occurred. Refreshing elements and retrying.")
+            continue
+    with csv_lock:
+        addToDB(gameName,gameId,gameLink,noItems,noRTUItems,itemName,createdBy,itemSize,postedTime,updatedTime,itemDesc,isCurated,isRTU,noUniqVis,nofavs,noSubs,rating,df) 
 
-    sendToDB(gameName,gameId,gameLink,noItems,noRTUItems,itemName,createdBy,itemSize,postedTime,updatedTime,itemDesc,isCurated,isRTU,noUniqVis,nofavs,noSubs,rating,df) 
-
-
-def sendToDB(gameName,gameId,gameLink,noItems,noRTUItems,itemName,createdBy,itemSize,postedTime,updatedTime,itemDesc,isCurated,isRTU,noUniqVis,nofavs,noSubs,rating,df):
+#adds entries to db
+def addToDB(gameName,gameId,gameLink,noItems,noRTUItems,itemName,createdBy,itemSize,postedTime,updatedTime,itemDesc,isCurated,isRTU,noUniqVis,nofavs,noSubs,rating,df):
     #adds row to db
     df.loc[len(df)] = [gameName,gameId,gameLink,noItems,noRTUItems,itemName,createdBy,itemSize,postedTime,updatedTime,itemDesc,isCurated,isRTU,noUniqVis,nofavs,noSubs,rating]
     df.to_csv('workshopGameScraper/workshopDB.csv', index=False)
 
+#worker function for multithreading
+def worker(gameUrls, start, end):
+    # Create a new instance of the Chrome driver
+    driver = webdriver.Chrome()
+    df = pd.read_csv('../workshopDB.csv')
+    for game in gameUrls[start:end]:
+        driver.get(game)
+        browseTab = driver.find_element(By.XPATH, '//*[@id="responsive_page_template_content"]/div[1]/div[1]/div[3]/div/div[2]/div[2]/a')
+        # Gets a list of all the links in the browse tab and grabs the ones that are links
+        browseList = [x + '&p=' for i, x in enumerate(browseTab.get_attribute('data-dropdown-html').split('\"')) if i % 2 == 1]
+        for tabLink in browseList:
+            driver.get(tabLink)
+            print(tabLink)
+            try:
+                numItems = driver.find_element(By.CLASS_NAME, 'workshopBrowsePagingInfo').text.split(' ')[-2]
+            except:
+                numItems = 0
+            print('Num of Items is: ', numItems)
+            gameItems = getItems(driver, tabLink)
+            print(gameItems)
+            print(len(gameItems))
+            if len(gameItems) != 0:
+                for item in gameItems:
+                    getItemInfo(driver, item, df, numItems)
+    driver.quit()
+
 # Create a new instance of the Chrome driver
 driver = webdriver.Chrome()
-df = pd.read_csv('workshopGameScraper/workshopDB.csv')
+df = pd.read_csv('../workshopDB.csv')
 driver.get("https://steamcommunity.com/workshop/?browsesort=Alphabetical&browsefilter=Alphabetical&p=1")
 
+# # Get the total number of pages
+# totalNumPages = int(driver.find_elements(By.CLASS_NAME, "workshop_apps_paging_pagelink")[-1].text)
 #gets the total number of games
-totalNumGames = driver.find_element(By.XPATH, "//*[@id=\"workshop_apps_total\"]").text
+totalNumGames = driver.find_element(By.XPATH, "//*[@id=\"workshop_apps_total\"]").text.split(',')
 #gets rid of the ',' in the number
-totalNumGames = int(totalNumGames[0:1] + totalNumGames[2:])
+totalNumGames = int(''.join(x for x in totalNumGames if x.isdigit()))
+print(totalNumGames)
 
-# gameUrls = getGameUrls(driver, totalNumPages)
-gameUrls = ['https://steamcommunity.com/app/1905530/workshop/']
 
-for game in gameUrls:
-    driver.get(game)
-    browseTab = driver.find_element(By.XPATH, '//*[@id="responsive_page_template_content"]/div[1]/div[1]/div[3]/div/div[2]/div[2]/a')
-    # Gets a list of all the links in the browse tab and grabs the ones that are links
-    browseList = [x + '&p=' for i, x in enumerate(browseTab.get_attribute('data-dropdown-html').split('\"')) if i % 2 == 1]
-    for tabLink in browseList:
-        driver.get(tabLink)
-        print(tabLink)
-        try:
-            numItems = driver.find_element(By.CLASS_NAME, 'workshopBrowsePagingInfo').text.split(' ')[-2]
-        except:
-            numItems = 0
-        print('Num of Items is: ', numItems)
-        gameItems = getItems(driver, tabLink)
-        print(gameItems)
-        print(len(gameItems))
-        if len(gameItems) != 0:
-            for item in gameItems:
-                getItemInfo(driver, item, df, numItems)
+gameUrls = getGameUrls(driver)
+print(gameUrls)
+# gameUrls = ['https://steamcommunity.com/app/1905530/workshop/']
+
+# Define the number of threads you want to use
+num_threads = 4
+# Create a lock to prevent multiple threads from writing to the CSV file simultaneously
+csv_lock = threading.Lock()
+# Split the gameUrls list into chunks for each thread
+chunk_size = totalNumGames // num_threads
+threads = []
+
+# Create and start worker threads
+for i in range(num_threads):
+    start = i * chunk_size
+    end = (i + 1) * chunk_size if i != num_threads - 1 else len(gameUrls)
+    thread = threading.Thread(target=worker, args=(gameUrls, start, end))
+    thread.start()
+    threads.append(thread)
+
+# Wait for all threads to finish
+for thread in threads:
+    thread.join()
+
+# for game in gameUrls:
+#     driver.get(game)
+#     browseTab = driver.find_element(By.XPATH, '//*[@id="responsive_page_template_content"]/div[1]/div[1]/div[3]/div/div[2]/div[2]/a')
+#     # Gets a list of all the links in the browse tab and grabs the ones that are links
+#     browseList = [x + '&p=' for i, x in enumerate(browseTab.get_attribute('data-dropdown-html').split('\"')) if i % 2 == 1]
+#     for tabLink in browseList:
+#         driver.get(tabLink)
+#         print(tabLink)
+#         try:
+#             numItems = driver.find_element(By.CLASS_NAME, 'workshopBrowsePagingInfo').text.split(' ')[-2]
+#         except:
+#             numItems = 0
+#         print('Num of Items is: ', numItems)
+#         gameItems = getItems(driver, tabLink)
+#         print(gameItems)
+#         print(len(gameItems))
+#         if len(gameItems) != 0:
+#             for item in gameItems:
+#                 getItemInfo(driver, item, df, numItems)
     
 
 # Print the final results
